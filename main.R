34132fb94390847a0229da3839eccb903e9c1468
Samuel Ackermann <saemi.ackermann@gmail.com>
2018-06-01 11:53:34 +0200

    example of how you can create a line chart

--- main.R
+++ main.R
@@ -8,4 +8,12 @@ source("util.R")
 
 # loadData('KO')
 allData <- loadData('SCMWY')
-data2017 <- subset(allData, as.Date(timestamp) > as.Date('2016-12-31') & as.Date(timestamp) < as.Date('2018-01-01'))
+
+# example of how you can create a line chart
+# data2017 <- subset(allData, as.Date(timestamp) > as.Date('2016-12-31') & as.Date(timestamp) < as.Date('2018-01-01'))
+# # convert column 'timestamp'
+# data2017$timestamp <- as.Date(data2017$timestamp)
+# # get the range for the y axis
+# yrange <- range(data2017$close)
+# plot(data2017$timestamp, data2017$close, type='l', ylim = yrange)
+# lines(trucks, type="o", pch=22, lty=2, col="red")
